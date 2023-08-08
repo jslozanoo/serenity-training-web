c3497b664a06c02dfaaa8a5835853a2cd5438cbd
Juan Lozano <juan.lozano.-ND2@disney.com>
2023-08-07 23:01:55 -0500

    Alerts completed

--- src/test/java/com/automation/WhenUser.java
+++ src/test/java/com/automation/WhenUser.java
@@ -1,11 +1,14 @@
 package com.automation;
 
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@ExtendWith(SerenityJUnit5Extension.class)
 public class WhenUser {
     @Managed
     WebDriver driver;
